7de0e1222c701af0c181eee94aa1ea6210e5e7bd
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2021-04-06 11:59:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { View, StyleSheet, Text,Image} from 'react-native';
+import GridImageView from 'react-native-grid-image-viewer';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        data: [],
+    }
+}
+componentDidMount() {
+  const apiUrl = "Your api key goes here";
+  fetch(apiUrl)
+          .then((response) => response.json())
+          .then((json) =>
+              this.setState({
+                  data: json,
+              })
+              //console.log(json)
+          )
+          .catch( (error) => {
+              console.log(error)
+          });
+}
+  render() {
+    const result = this.state;
+        var images = result.data.map((data) => {
+            return {
+                "image" : data.urls.regular
+            }
+        });
+    return (
+      <View style={styles.background}>
+      <Image source={require('./assets/picopedia.png')} style={styles.logo}/>  
+      <Text style={styles.headline_text}>PicoPedia</Text>
+      <Text style={styles.explore_text}>Unsplash : Beautiful high-quality photos curated by Alik.</Text>
+      <GridImageView data={images}/>
+    </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'black',
+    flex: 1
+  },
+  headline_text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 0,
+    textAlign:'center',
+    marginLeft: 20
+  },
+  explore_text: {
+    marginTop: 5,
+    marginBottom: 10,
+    color: 'white',
+    marginLeft: 20,
+    fontSize: 12,
+    textAlign:'center',
+    fontWeight: '600'
+  },
+  logo:{
+    height:70,
+    width:70,
+    marginLeft:'auto',
+    marginRight:'auto',
+    marginTop:10,
+  }
+});
